e5cc2c53c452a08371d0d733a5baf46560ef8b98
dmitriyl99 <dmitriyl1899@gmail.com>
2021-09-04 23:46:25 +0500

    Initial kommit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:event_flats/screens/login.screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Event Flats',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {LoginScreen.route: (context) => new LoginScreen()},
+      initialRoute: LoginScreen.route,
+    );
+  }
+}
---
+++ lib/screens/login.screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String route = '/login';
+
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
