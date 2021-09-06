103d91093137387ef34af87e21efc47d59732398
dmitriyl99 <dmitriyl1899@gmail.com>
2021-09-06 17:51:24 +0500

    Add hive database

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:event_flats/models/user.dart';
 import 'package:event_flats/services/authentication.dart';
 import 'package:event_flats/view/resources/colors.dart';
 import 'package:event_flats/view/screens/flats/list.screen.dart';
 import 'package:event_flats/view/screens/login.screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Hive.initFlutter();
+  Hive.registerAdapter(new UserAdapter());
   runApp(MyApp());
 }
 
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:hive/hive.dart';
+
 class User {
   final String displayName;
   final String email;
@@ -5,11 +9,24 @@ class User {
 
   User(this.displayName, this.email, this.isAdmin);
 
-  factory User.fromJson(Map<String, dynamic> json) => User(
-      json['displayName'] as String,
-      json['email'] as String,
-      json['isAdmin'] as bool);
+  factory User.fromJson(dynamic json) => User(json['displayName'] as String,
+      json['email'] as String, json['isAdmin'] as bool);
 
   Map<String, dynamic> toJson() =>
       <String, dynamic>{'email': email, 'isAdmin': isAdmin};
+}
+
+class UserAdapter extends TypeAdapter<User> {
+  @override
+  final typeId = 0;
+
+  @override
+  User read(BinaryReader reader) {
+    return User.fromJson(json.decode(reader.read()));
+  }
+
+  @override
+  void write(BinaryWriter writer, User obj) {
+    writer.writeString(json.encode(obj.toJson()));
+  }
 }
