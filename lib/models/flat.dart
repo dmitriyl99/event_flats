e838773ca29fe27e0bb2ea89d9b97befac75f565
dmitriyl99 <dmitriyl1899@gmail.com>
2021-09-08 15:49:14 +0500

    Fix flat model

--- lib/models/flat.dart
+++ lib/models/flat.dart
@@ -1,15 +1,14 @@
-enum FlatRepairType {
-  Clean,
-}
-
 class Flat {
   late String id;
   final String name;
+  final double price;
   final int floor;
   final int numberOfFloors;
   final int numberOfRooms;
-  final FlatRepairType flatRepairType;
+  final String flatRepair;
+  final bool isFavorite;
+  final DateTime createdAt;
 
-  Flat(this.name, this.floor, this.numberOfFloors, this.numberOfRooms,
-      this.flatRepairType);
+  Flat(this.name, this.price, this.floor, this.numberOfFloors,
+      this.numberOfRooms, this.flatRepair, this.createdAt, this.isFavorite);
 }
