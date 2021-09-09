6de0684745fd6796940ee857b6b653a794d3f5ea
dmitriyl99 <dmitriyl1899@gmail.com>
2021-09-09 22:35:53 +0500

    Area, address, description, owner

--- lib/models/flat.dart
+++ lib/models/flat.dart
@@ -6,9 +6,26 @@ class Flat {
   final int numberOfFloors;
   final int numberOfRooms;
   final String flatRepair;
+  final double area;
+  final String address;
+  final String description;
+  final String? ownerName;
+  final String ownerPhone;
   final bool isFavorite;
   final DateTime createdAt;
 
-  Flat(this.name, this.price, this.floor, this.numberOfFloors,
-      this.numberOfRooms, this.flatRepair, this.createdAt, this.isFavorite);
+  Flat(
+      this.name,
+      this.price,
+      this.floor,
+      this.numberOfFloors,
+      this.numberOfRooms,
+      this.flatRepair,
+      this.createdAt,
+      this.isFavorite,
+      this.area,
+      this.description,
+      this.address,
+      this.ownerPhone,
+      {this.ownerName});
 }
--- lib/models/repositories/flats_fake_repository.dart
+++ lib/models/repositories/flats_fake_repository.dart
@@ -5,13 +5,27 @@ class FakeFlatsRepository extends FlatsRepository {
   @override
   Future<List<Flat>> getFlats() async {
     return <Flat>[
-      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), false),
-      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), false),
-      Flat('Яккасарай', 30000, 4, 5, 2, 'Чистый', DateTime.now(), false),
-      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), true),
-      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), false),
-      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), true),
-      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), false)
+      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), false, 48.3,
+          "Lorem ipsum, dolor sit amet", 'Ашхабад', '+998909879930',
+          ownerName: 'Дмитрий'),
+      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), false, 48.3,
+          "Lorem ipsum, dolor sit amet", 'Ашхабад', '+998909879930',
+          ownerName: 'Дмитрий'),
+      Flat('Яккасарай', 30000, 4, 5, 2, 'Чистый', DateTime.now(), false, 48.3,
+          "Lorem ipsum, dolor sit amet", 'Ашхабад', '+998909879930',
+          ownerName: 'Дмитрий'),
+      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), true, 48.3,
+          "Lorem ipsum, dolor sit amet", 'Ашхабад', '+998909879930',
+          ownerName: 'Дмитрий'),
+      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), false, 48.3,
+          "Lorem ipsum, dolor sit amet", 'Ашхабад', '+998909879930',
+          ownerName: 'Дмитрий'),
+      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), true, 48.3,
+          "Lorem ipsum, dolor sit amet", 'Ашхабад', '+998909879930',
+          ownerName: 'Дмитрий'),
+      Flat('Яккасарай', 20000, 4, 5, 2, 'Чистый', DateTime.now(), false, 48.3,
+          "Lorem ipsum, dolor sit amet", 'Ашхабад', '+998909879930',
+          ownerName: 'Дмитрий')
     ];
   }
 }
