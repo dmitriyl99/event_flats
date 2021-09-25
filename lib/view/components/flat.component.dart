b4a6677d24f2d640a3e9621dc8bf98541f0d834f
dmitriyl99 <dmitriyl1899@gmail.com>
2021-09-25 16:17:55 +0500

    Add inkwell and gesture detector for favorite

--- lib/view/components/flat.component.dart
+++ lib/view/components/flat.component.dart
@@ -1,5 +1,6 @@
 import 'package:event_flats/helpers/number_formatting.dart';
 import 'package:event_flats/models/flat.dart';
+import 'package:event_flats/models/repositories/flats_repository.dart';
 import 'package:event_flats/view/resources/colors.dart';
 import 'package:event_flats/view/screens/flats/show.screen.dart';
 import 'package:flutter/material.dart';
@@ -8,8 +9,11 @@ class FlatComponent extends StatelessWidget {
   final Flat flat;
   final Function onEditCallback;
 
-  const FlatComponent(this.flat, this.onEditCallback, {Key? key})
+  const FlatComponent(this.flat, this._flatsRepository, this.onEditCallback,
+      {Key? key})
       : super(key: key);
+
+  final FlatsRepository _flatsRepository;
 
   @override
   Widget build(BuildContext context) {
@@ -18,44 +22,55 @@ class FlatComponent extends StatelessWidget {
       child: Card(
         elevation: 4,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  flat.isFavorite
-                      ? 'assets/house_favorite.png'
-                      : 'assets/house.png',
-                  height: 32,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      flat.address,
-                      style: TextStyle(fontSize: 18),
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(FlatShowScreen.route, arguments: flat);
+          },
+          child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () async {
+                      await _flatsRepository.toggleFavorite(flat.id);
+                    },
+                    child: Image.asset(
+                      flat.isFavorite
+                          ? 'assets/house_favorite.png'
+                          : 'assets/house.png',
+                      height: 32,
                     ),
-                    Text(
-                        '${flat.numberOfRooms}/${flat.floor}/${flat.numberOfFloors}'),
-                    Text(flat.flatRepair)
-                  ],
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: AppColors.primaryColor,
-                      borderRadius: BorderRadius.circular(10)),
-                  padding: EdgeInsets.all(5),
-                  child: Text(
-                    NumberFormattingHelper.currency(flat.price),
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),
                   ),
-                ),
-              ],
-            )),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        flat.address,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                          '${flat.numberOfRooms}/${flat.floor}/${flat.numberOfFloors}'),
+                      Text(flat.flatRepair)
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: AppColors.primaryColor,
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: EdgeInsets.all(5),
+                    child: Text(
+                      NumberFormattingHelper.currency(flat.price),
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ),
+                ],
+              )),
+        ),
         // child: ListTile(
         //   leading: Image.asset(
         //     flat.isFavorite ? 'assets/house_favorite.png' : 'assets/house.png',
