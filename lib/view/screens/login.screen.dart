059922839a0fef3b06167ecde109f3bed37d4383
dmitriyl99 <dmitriyl1899@gmail.com>
2023-08-07 12:53:31 +0500

    Fix login page

--- lib/view/screens/login.screen.dart
+++ lib/view/screens/login.screen.dart
@@ -129,7 +129,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return RichText(
       textAlign: TextAlign.center,
       text: TextSpan(
-          text: 'Rus',
+          text: 'Sul',
           style: TextStyle(
             fontSize: 30,
             fontWeight: FontWeight.w700,
@@ -137,7 +137,7 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           children: [
             TextSpan(
-              text: 'lan F',
+              text: 'tan F',
               style: TextStyle(color: Colors.white, fontSize: 30),
             ),
             TextSpan(
